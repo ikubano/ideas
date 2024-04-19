35b5987be37e8031ff4de812b2d24befe8fb5375
ikubano <ianmwanzi0@gmail.com>
2024-04-19 17:13:16 +0300

    second commit

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ if (close) {
         nav.classList.remove('active');
     })
 }
+document.addEventListener('DOMContentLoaded', function() {
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbykNkaWwt_zgeLPHuYgE7reTv000jCzPAjlWPDkygtCIUMe2BEt85oMutAH1EKGPkL6/exec'
+    const form = document.forms['submit-to-google-sheet']
+
+    const msg = document.getElementById("msg")
+
+    form.addEventListener('submit', e => {
+        e.preventDefault()
+        fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+            .then(response => {
+                msg.innerHTML = "Message was sent successfully. I'll Attend to you shortly!"
+                setTimeout(function (){
+                    msg.innerHTML = ""
+                },5000)
+                form.reset()
+            })
+            .catch(error => console.error('Error!', error.message))
+    })
+})
+
+
